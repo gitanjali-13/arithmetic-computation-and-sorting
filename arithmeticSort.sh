a81b901d43a145e24ba692ca2a3b81a479237722
gitanjali-13 <gitaanjalide@gmail.com>
2020-05-18 13:37:43 +0530

    UC6 added

--- arithmeticSort.sh
+++ arithmeticSort.sh
@@ -9,3 +9,12 @@ e=$(($a*$b+$c))
 f=$(($c+$a/$b))
 g=$(($a%$b+$c))
 
+result[a+b*c]="$d"
+result[a*b+c]="$e"
+result[c+a/b]="$f"
+result[a%b+c]="$g"
+
+echo "Result of first computation: " ${result[a+b*c]}
+echo "Result of second computation: " ${result[a*b+c]}
+echo "Result of third computation : " ${result[c+a/b]}
+echo "Result of forth computation: " ${result[a%b+c]}
